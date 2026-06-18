30cdadda99c4fff3b2e853e691e1a31b640397c5
djordjedjordjevic11 <djordje253djordjevic@gmail.com>
2026-06-18 19:49:00 +0200

    added BrojiloDTO

--- app/DTO/BrojiloDto.cs
+++ app/DTO/BrojiloDto.cs
@@ -1,6 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 namespace app.DTO
 {
     public class BrojiloDto
     {
+        public string SerijskiBroj { get; set; }
+        public DateTime DatumInstalacije { get; set; }
+        public string Status { get; set; }
+        public string Lokacija { get; set; }
+        public decimal? KoeficijentMnozenja { get; set; }
+
+        public string Tipovi { get; set; }
+        public string Potrosaci { get; set; }
+        public string DatumiZamene { get; set; }
     }
 }
